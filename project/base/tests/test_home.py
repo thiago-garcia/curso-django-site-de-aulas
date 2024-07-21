f1ca02155d6bb32187ae7cddc50883c6f611c505
Thiago Garcia <thiago-garcia@outlook.com>
2024-07-21 17:52:22 -0300

    Criado template da home e testes close #19

--- project/base/tests/test_home.py
+++ project/base/tests/test_home.py
@@ -1,8 +1,30 @@
-from django.test import Client
 from django.urls import reverse
+import pytest
+
+from project.django_assertions import assert_contains
 
 
-def test_status_code():
-    client = Client()
+@pytest.fixture
+def resp(client):
     resp = client.get(reverse('base:home'))
+    return resp
+
+
+def test_status_code(resp):
     assert resp.status_code == 200
+
+
+def test_title(resp):
+    assert_contains(resp, '<title>Site de aulas - Comece agora mesmo!</title>')
+
+
+def test_bloco_comece_agora(resp):
+    assert_contains(resp, '<h2>Comece agora mesmo!</h2>')
+
+
+def test_bloco_nossa_historia(resp):
+    assert_contains(resp, '<h2>Conheça nossa história</h2>')
+
+
+def test_email_link(resp):
+    assert_contains(resp, 'href="mailto:email@email.com"')
