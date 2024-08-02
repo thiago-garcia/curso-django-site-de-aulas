ed6962fccff5a1a1a73153ccafaa770395def9c1
Thiago Garcia <thiago-garcia@outlook.com>
2024-08-01 22:58:04 -0300

    Adicionado a busca pelos módulos e aulas para formação do índice e ajustado o template
    Close #28

--- project/modulos/facade.py
+++ project/modulos/facade.py
@@ -1,5 +1,6 @@
 from typing import List
-from project.modulos.models import Modulo
+from project.modulos.models import Modulo, Aula
+from django.db.models import Prefetch
 
 
 def listar_modulos_ordenados() -> List[Modulo]:
@@ -9,3 +10,11 @@ def listar_modulos_ordenados() -> List[Modulo]:
     """
 
     return list(Modulo.objects.order_by('order').all())
+
+
+def listar_modulos_com_aulas():
+    """
+    Lista módulos com as aulas
+    """
+    aulas_ordenadas = Aula.objects.order_by('order').all()
+    return Modulo.objects.order_by('order').prefetch_related(Prefetch('aula_set', aulas_ordenadas, 'aulas')).all()
--- project/modulos/views.py
+++ project/modulos/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from project.modulos import facade
 
 
 def indice(request):
-    return render(request, 'modulos/indice.html')
+    ctx = {'modulos': facade.listar_modulos_com_aulas()}
+    return render(request, 'modulos/indice.html', ctx)
 
 
 def detalhe(request, slug):
